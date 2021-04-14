a9868b6ddedd2a9432f9abb63021ac8eff3ee31f
on work work <tejas1188245@gmail.com>
2021-04-14 14:43:40 +0530

    completed project

--- number-finder/script.js
+++ number-finder/script.js
@@ -14,8 +14,41 @@ recognition.start();
 function onSpeack(e){
     const msg = e.results[0][0].transcript;
 
- //   writeMessage(msg);
-  //  checkNumber(msg);
+   writeMessage(msg);
+    checkNumber(msg);
+
+}
+function writeMessage(msg)
+{
+    msgEl.innerHTML = `
+    <div>You said:</div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+function checkNumber(msg){
+    const num =  +msg;
+
+    //check if is valid number
+    if(Number.isNaN(num)){
+        msgEl.innerHTML += '<div>That is Not a Valid Number</div>';
+        return;
+    }
+    //check in rang 
+    if(num >100 || num<1){
+        msgEl.innerHTML +='<div>Number Must be between 1 and 100</div>';
+    return;
+    }
+
+    if(num === randomNum){
+        document.body.innerHTML = `<h2>Congrats! You have guessed the number ! <br> <br>It was ${num}  </h2> <button class="play-again" id="play-again">Play Again</button>`;
+
+    }
+    else if(num>randomNum){
+        msgEl.innerHTML += '<div>GO LOWER</div>';
+    }else{
+        msgEl.innerHTML += '<div>Go HIGHER</div>'
+    }
 }
 
 function getRandomNumber(){
@@ -26,6 +59,15 @@ function getRandomNumber(){
 //speak result
 recognition.addEventListener('result',onSpeack);
 
-/*<div>You said:</div>
-<span class="box">20</span>
-<div>Go HIGHER</div>*/
+
+/*end sr service*/
+
+recognition.addEventListener('end', () =>recognition.start());
+
+
+document.body.addEventListener('click', (e) =>{
+    if(e.target.id == 'play-again'){
+        window.location.reload();
+
+    }
+});
