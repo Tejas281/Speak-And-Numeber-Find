f86c6c3dfb5b064796ff41e293f4ecee8b28eef7
on work work <tejas1188245@gmail.com>
2021-04-14 13:11:01 +0530

    completed front End

---
+++ number-finder/script.js
@@ -0,0 +1,13 @@
+
+const msgEl = document.getElementById('msg');
+const randomNum = getRandomNumber();
+console.log(randomNum); 
+function getRandomNumber(){
+    return Math.floor(Math.random () * 100) + 1;
+
+}
+
+
+/*<div>You said:</div>
+<span class="box">20</span>
+<div>Go HIGHER</div>*/
