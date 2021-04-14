700e1078c71a985ffe391c8619e53836607a2a8f
on work work <tejas1188245@gmail.com>
2021-04-14 14:06:11 +0530

    javascript add and voice are fetch and diplay

--- number-finder/script.js
+++ number-finder/script.js
@@ -1,12 +1,30 @@
 
 const msgEl = document.getElementById('msg');
 const randomNum = getRandomNumber();
-console.log(randomNum); 
+console.log('Number:',randomNum); 
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+ 
+//start recognition and game
+recognition.start();
+
+//Capture User Speak
+function onSpeack(e){
+    const msg = e.results[0][0].transcript;
+
+ //   writeMessage(msg);
+  //  checkNumber(msg);
+}
+
 function getRandomNumber(){
     return Math.floor(Math.random () * 100) + 1;
 
 }
 
+//speak result
+recognition.addEventListener('result',onSpeack);
 
 /*<div>You said:</div>
 <span class="box">20</span>
